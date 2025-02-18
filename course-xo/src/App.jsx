8e6c8a77201c8104200cff4042f4a622db497c76
mznmo <102366479+mznmo@users.noreply.github.com>
2025-02-18 17:39:36 +0200

    board + symbols + highlight

--- course-xo/src/App.jsx
+++ course-xo/src/App.jsx
@@ -1,9 +1,26 @@
-
+import { useState } from 'react';
+import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare(){
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X'? 'O' : 'X');
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id='game-container'>
+      <ol id='players' className='highlight-player'>
+        <Player name={'Player 1'} symbol='X' isActive={activePlayer === 'X'}/>
+        <Player name={'Player 2'} symbol='O' isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard onSelectCell={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      </div>
+   
+      LOG
+
+    </main> 
   )
 }
 
---
+++ course-xo/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({name, symbol, isActive}){
+
+    const [playerName, setPlayerName] = useState(name);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEdit(editStatus){
+        setIsEditing(editStatus);
+    }
+
+    return(
+          <li className={isActive ? 'active' : undefined}>
+            <span className='player-symbol'>{symbol}</span>
+            <span className='player'>
+            {/* Updating old player name to the new name */}
+            {!isEditing ? <span className='player-name'>{playerName}</span>
+            : <input type='text' value={playerName} onChange={(e)=>setPlayerName(e.target.value)} />}
+            {/* Disabling the saving feature if the input is empty */}
+            {isEditing ? (<button onClick={() => {
+            if (playerName.trim() !== '') {
+                handleEdit(false);
+            }
+            }}
+            >
+            Save
+            </button>
+            ) : (<button onClick={() => handleEdit(true)}>Edit</button>)}
+            </span>
+          </li>
+    );
+}
